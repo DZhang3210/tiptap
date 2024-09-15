b1f6aa9ea0e15653b55fc8fa4484236abc26f6fb
DZhang3210 <davidzhang3210@gmail.com>
2024-09-15 17:33:05 -0400

    new commit

--- src/components/EnterKeyHandler.tsx
+++ src/components/EnterKeyHandler.tsx
@@ -21,14 +21,8 @@ export const EnterKeyHandler = ({
     addKeyboardShortcuts() {
       return {
         Enter: () => {
-          // fetch("https://jsonplaceholder.typicode.com/todos/1")
-          //   .then((response) => response.json())
-          //   .then((json) => console.log(json));
-          // console.log("THING", thing);
-          // console.log("TEXT", thing);
           ref.current?.click();
-          // You can place your custom logic here
-          // Return `true` to prevent the default behavior, `false` to allow it
+
           return true;
         },
       };
